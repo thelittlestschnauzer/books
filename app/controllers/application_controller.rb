a24311289da3ceed81cf066d52bcb80b27024681
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2018-01-21 22:11:18 -0500

    set up application controller for welcome page and set up the style of the welcome page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, "password_security"
   end
 
   get "/" do
     erb :welcome
   end
+
 
 end
