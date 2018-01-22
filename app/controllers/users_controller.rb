893be29d1e31ac5b2f4a5450822e0d87560e68a2
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2018-01-21 22:32:27 -0500

    set up the get and post action for sign up feature in user controller and tested

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
-end 
+
+  get '/signup' do
+    if !logged_in?
+      erb :'/user/signup'
+    else
+      redirect to "/books"
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if @user.save
+      session[:user_id] = @user.id
+      redirect to '/books'
+    else
+      redirect to '/signup'
+    end
+  end
+end
