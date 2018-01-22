d851a27eb91bf53dd5d7932d4bdcf30f4673c8a4
the littlest schnauzer <thelittlestschnauzer@gmail.com>
2018-01-21 21:35:02 -0500

    created tables and models for user and book

---
+++ db/migrate/20180122023136_create_books.rb
@@ -0,0 +1,9 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :description
+      t.integer :user_id
+    end
+  end
+end
